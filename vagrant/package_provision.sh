0ffe9eaeb7e8ba91134f3589da586dbac5e6e749
ermueller <erik@east.de>
2015-11-26 03:06:45 +0100

    Added datastore and datapusher functionality

--- vagrant/package_provision.sh
+++ vagrant/package_provision.sh
@@ -1,38 +1,38 @@
-echo "this shell script is going to setup a running ckan instance based on the CKAN 2.4 packages"
+echo "This shell script is going to setup a running ckan instance based on the CKAN 2.4 packages"
 
-echo "switching the OS language"
+echo "Switching the OS language"
 locale-gen
 export LC_ALL="en_US.UTF-8"
 sudo locale-gen en_US.UTF-8
 
-echo "updating the package manager"
+echo "Updating the package manager"
 sudo apt-get update
 
-echo "installing dependencies available via apt-get"
+echo "Installing dependencies available via apt-get"
 sudo apt-get install -qq -y nginx apache2 libapache2-mod-wsgi libpq5
 
-echo "downloading the CKAN package"
-wget -q http://packaging.ckan.org/python-ckan_2.4-precise_amd64.deb
+echo "Downloading the CKAN package"
+sudo wget -q http://packaging.ckan.org/python-ckan_2.4-precise_amd64.deb
 
-echo "installing the CKAN package"
-sudo dpkg -i python-ckan_2.4-precise_amd64.deb
+echo "Installing the CKAN package"
+sudo dpkg -i /vagrant/vagrant/python-ckan_2.4-precise_amd64.deb
 
 echo "Preventing NGINX from being started on a reboot"
 sudo update-rc.d -f nginx disable
 
-echo "changing the apache configuration back to port 80"
+echo "Changing the apache configuration back to port 80"
 sudo cp /vagrant/vagrant/package_ports.conf /etc/apache2/ports.conf
 sudo cp /vagrant/vagrant/package_ckan_default.conf /etc/apache2/sites-available/ckan_default.conf
 sudo service apache2 restart
 
-echo "install postgresql and jetty"
+echo "Installing postgresql and jetty"
 sudo apt-get install -y postgresql solr-jetty openjdk-6-jdk
 
-echo "copying jetty configuration"
+echo "Copying jetty configuration"
 sudo cp /vagrant/vagrant/jetty /etc/default/jetty
 sudo service jetty start
 
-echo "linking the solr schema file"
+echo "Linking the solr schema file"
 sudo mv /etc/solr/conf/schema.xml /etc/solr/conf/schema.xml.bak
 sudo ln -s /usr/lib/ckan/default/src/ckan/ckanext/multilingual/solr/dutch_stop.txt /etc/solr/conf/dutch_stop.txt
 sudo ln -s /usr/lib/ckan/default/src/ckan/ckanext/multilingual/solr/english_stop.txt /etc/solr/conf/english_stop.txt
@@ -50,7 +50,7 @@ sudo ln -s /usr/lib/ckan/default/src/ckan/ckanext/multilingual/solr/schema.xml /
 
 sudo service jetty restart
 
-echo "create a CKAN database in postgresql"
+echo "Creating a CKAN database in postgresql"
 sudo -u postgres createuser -S -D -R ckan_default
 sudo -u postgres psql -c "ALTER USER ckan_default with password 'pass'"
 sudo -u postgres createuser -S -D -R datastore_default
@@ -58,13 +58,15 @@ sudo -u postgres psql -c "ALTER USER datastore_default with password 'pass'"
 sudo -u postgres createdb -O ckan_default datastore_default -E utf-8
 sudo -u postgres createdb -O ckan_default ckan_default -E utf-8
 sudo cp /vagrant/vagrant/package_production.ini /etc/ckan/default/production.ini
+
+sudo bash /vagrant/vagrant/viewscript.sh
+
 sudo ckan datastore set-permissions | sudo -u postgres psql --set ON_ERROR_STOP=1
 
-
-echo "initialize CKAN database"
+echo "Initializing CKAN database"
 sudo ckan db init
 
-echo "enabling filestore with local storage"
+echo "Enabling filestore with local storage"
 sudo mkdir -p /var/lib/ckan/default
 sudo chown www-data /var/lib/ckan/default
 sudo chmod u+rwx /var/lib/ckan/default
@@ -72,15 +74,14 @@ sudo chown --recursive www-data /etc/ckan/default/
 sudo chmod --recursive u+rwx /etc/ckan/default/
 sudo a2dissite default
 sudo service apache2 reload
-sudo service apache2 restart
 
-echo "creating an admin user"
+echo "Creating an admin user"
 source /usr/lib/ckan/default/bin/activate
 cd /usr/lib/ckan/default/src/ckan
 paster --plugin=ckan user add admin email=admin@email.org password=pass -c /etc/ckan/default/production.ini
 paster --plugin=ckan sysadmin add admin -c /etc/ckan/default/production.ini
 
-echo "loading some multilingual test data"
-paster --plugin=ckan create-test-data translations -c /etc/ckan/default/production.ini
+echo "Loading some test data"
+paster --plugin=ckan create-test-data -c /etc/ckan/default/production.ini
 
 echo "you should now have a running instance on http://ckan.lo"
---
+++ vagrant/viewscript.sh
@@ -0,0 +1,21 @@
+echo "Installing git and cloning repositories"
+source /usr/lib/ckan/default/bin/activate
+cd /usr/lib/ckan/default/src
+sudo apt-get install -qq -y git
+sudo git clone -q https://github.com/ckan/ckanext-viewhelpers
+sudo git clone -q https://github.com/ckan/ckanext-basiccharts
+sudo git clone -q https://github.com/ckan/ckanext-dashboard
+
+echo "Installing pip (needed for additional views)"
+sudo apt-get install -qq -y python-pip
+
+
+echo "Installing python setuptools"
+sudo wget -q https://bootstrap.pypa.io/ez_setup.py
+sudo python ez_setup.py > /dev/null
+sudo rm *.zip
+
+echo "Installing views"
+pip install ./ckanext-viewhelpers
+pip install ./ckanext-basiccharts
+pip install ./ckanext-dashboard
